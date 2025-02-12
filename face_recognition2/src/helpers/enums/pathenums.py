9cd594ec9cbc67f53a7d1dba301c0bace1f6d53e
00JIMMY00 <jimmygamaljimmy@gmail.com>
2025-02-12 16:55:58 +0200

    Refactor path definitions in PathEnums for consistency and clarity

--- face_recognition2/src/helpers/enums/pathenums.py
+++ face_recognition2/src/helpers/enums/pathenums.py
@@ -4,7 +4,7 @@ import os
 current_directory = os.getcwd()
 
 class PathEnums(Enum):
-    INPUT_DIR = os.path.join(current_directory, "face_recognition2/src/assets", "captured_images")
-    OUTPUT_DIR = os.path.join(current_directory, "face_recognition2/src/assets", "captured_images")
-    model_path = os.path.join(current_directory, "face_recognition2/src/assets", "face_recognition_model.pkl")
-    label_map_path = os.path.join(current_directory, "face_recognition2/src/assets", "label_map.pkl")
+    INPUT_DIR = os.path.join(current_directory, "face_recognition2", "src", "assets", "captured_images")
+    OUTPUT_DIR = os.path.join(current_directory, "face_recognition2", "src", "assets", "captured_images")
+    model_path = os.path.join(current_directory, "face_recognition2", "src", "assets", "face_recognition_model.pkl")
+    label_map_path = os.path.join(current_directory, "face_recognition2", "src", "assets", "label_map.pkl")
--- routers/pathEnums.py
+++ routers/pathEnums.py
@@ -5,5 +5,5 @@ current_directory = os.getcwd()
 
 
 class PathEnums(Enum):
-    facetemplate = os.path.join(current_directory, "templates/face.html")
+    facetemplate = os.path.join(current_directory, "templates","face.html")
     # print(facetemplate)  # Output: /path/to/face_recognition2/templates/face.html
